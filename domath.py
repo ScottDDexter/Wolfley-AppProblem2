fad5cc02167cf2b6d76fa5a7eadcaaab23b88c99
David Wolfley <113908176+DavidWolfley@users.noreply.github.com>
2022-11-04 16:33:51 -0400

    Add files via upload

---
+++ domath.py
@@ -0,0 +1,10 @@
+def main():
+    def domath(number):
+        print("hi")
+        print(str(number) + " + " + str(number) + " = " + str(number + number))
+
+    domath(5)
+
+
+if __name__ == '__main__':
+    main()
